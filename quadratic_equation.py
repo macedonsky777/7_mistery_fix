a588a9144e584795bceaf7c53d5d2d98966ca729
macedonsky777 <yano.motoharu.kikuchi@gmail.com>
2017-09-04 18:04:37 +0300

    исправлена библиотека и условия

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,11 +1,14 @@
-from math import sqrt
+import cmath
+from cmath import sqrt
 
 
-def get_roots(a, b, c):
+def get_roots(a,b,c):
     discriminant = b ** 2 - 4 * a * c
     root1 = (-b - sqrt(discriminant)) / (2 * a)
     root2 = (-b + sqrt(discriminant)) / (2 * a)
-    if discriminant == 0:
+    if discriminant > 0:
+        return root1, root2
+    elif discriminant == 0:
         return root1, None
     else:
-        return root1, root2
+        return None, None
